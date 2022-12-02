c86eeee446a5c565add78956217f2c8ec6f844c9
mbelinga777 <belingam@oregonstate.edu>
2022-12-01 18:11:57 -0800

    basic functions

---
+++ public/index.js
@@ -0,0 +1,34 @@
+var theExercises=Array.from(document.querySelectorAll())
+var theRecipes=Array.from(document.querySelectorAll())
+function addExercise(photoURL,muscleGroup,muscle,type,name,description)
+{
+
+}
+function addRecipe(photoURL,name,calories,protein,sugar,carbs,fat,link)
+{
+
+}
+function filterExercise(muscleGroup,muscle,type)
+{
+
+}
+function filterRecipes(alories,protein,sugar,carbs,fat)
+{
+
+}
+function closeExercise()
+{
+    
+}
+function clearExercises()
+{
+    
+}
+function clearRecipes()
+{
+
+}
+function closeFoodMenu()
+{
+
+}
