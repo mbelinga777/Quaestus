fcc687488a77ef09c5ad652d901a1dc85cda4566
mbelinga777 <belingam@oregonstate.edu>
2022-12-03 18:53:44 -0800

    new

--- public/index.js
+++ public/index.js
@@ -1,6 +1,7 @@
+/*
 var AllExercises=Array.from(document.querySelectorAll())
 var AllRecipes=Array.from(document.querySelectorAll())
-
+*/
 function addRecipe(photoURL,name,calories,protein,sugar,carbs,fat,link)
 {
 
@@ -31,17 +32,21 @@ function toggleExerciseFilter()
   
 function clearExercises()
 {
+    /*
     var posts=document.getElementById('exercisePosts')
     while(posts.firstChild)
     {
       posts.removeChild(posts.firstChild)
     }
+    */
 }
 function clearRecipes()
 {
+    /*
     var posts=document.getElementById('recipePosts')
     while(posts.firstChild)
     {
        posts.removeChild(posts.firstChild)
     } 
+    */
 }
