9b15a6c3786bcce00ce56223995a6ae3fe30f565
mbelinga777 <belingam@oregonstate.edu>
2022-12-03 17:43:55 -0800

    edit filter

--- public/index.js
+++ public/index.js
@@ -1,17 +1,24 @@
 var AllExercises=Array.from(document.querySelectorAll())
 var AllRecipes=Array.from(document.querySelectorAll())
-function addExercise(photoURL,muscleGroup,muscle,type,name,description)
-{
 
-}
 function addRecipe(photoURL,name,calories,protein,sugar,carbs,fat,link)
 {
 
 }
-function filterExercise(muscle,type)
+function filterExercises(muscle,type)
 {
     clearExercises()
 
+}
+function filterExercisesByname(name)
+{
+    clearExercises()
+
+
+}
+function filterRecipes(calories)
+{
+    clearRecipes()
 }
 function filterRecipes(calories)
 {
@@ -21,13 +28,24 @@ function closeExercise()
 {
     
 }
-function closeFoodMenu()
+function closeFoodFilter()
 {
-
+    var modalB=document.getElementById('modal-backdrop')
+    var foodFilter=document.getElementById('sell-something-modal')
+    modalB.classList.add('hidden')
+    foodFilter.classList.add('hidden')
+    var text=document.getElementById('post-calories-input')
+    text.value=""
+    var textPhoto=document.getElementById('post-photo-input')
+    textPhoto.value=""
+    var textPrice=document.getElementById('post-price-input')
+    textPrice.value=""
+    var textCity=document.getElementById('post-city-input')
+    textCity.value=""
 }
 function clearExercises()
 {
-    var posts=document.getElementById('ExercisePosts')
+    var posts=document.getElementById('exercisePosts')
     while(posts.firstChild)
     {
       posts.removeChild(posts.firstChild)
