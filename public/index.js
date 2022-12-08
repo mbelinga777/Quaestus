c91a289955deaa4d24d97ec4de43e108000b5c0c
mbelinga777 <belingam@oregonstate.edu>
2022-12-07 16:09:09 -0800

    working on filter

--- public/index.js
+++ public/index.js
@@ -1,7 +1,15 @@
-/*
-var AllExercises=Array.from(document.querySelectorAll())
-var AllRecipes=Array.from(document.querySelectorAll())
-*/
+
+var videoArray=[];
+//var AllRecipes=Array.from(document.querySelectorAll())
+function insertVideo(name, videoURL,type) {
+    var videoPost = Handlebars.templates.exerciseElement({
+      name: name,
+      videoURL: videoURL,
+      type: type,
+    })
+    var videoSection = document.querySelector('#exercise-videos');
+    videoSection.insertAdjacentHTML("beforeend", videoPost);
+  }
 function addRecipe(photoURL,name,calories,protein,sugar,carbs,fat,link)
 {
 
@@ -10,28 +18,18 @@ function addRecipe(photoURL,name,calories,protein,sugar,carbs,fat,link)
     this function filters all the exercises based on what boxes the user checked
 
 */
-function filterExercises(muscle,type)
+
+function filterExercises(AllExercises)
 {
-    clearExercises()
+    
 
 }
+
 function filterExercisesByname(name)
 {
     clearExercises()
 }
-var filterExButton = document.getElementById('filter-update-button')
-filterExButton.addEventListener('click',function(){
 
-    var conditionType= document.querySelectorAll('input[name="filter-condition"]')
-    var values = [];
-    for (var i=0;i<5;i++) 
-    {
-      if (conditionType[i].checked) 
-      {
-        values.push(conditionType[i].value)
-      }
-    }
-})
 function filterRecipes(calories)
 {
     clearRecipes()
@@ -64,3 +62,54 @@ function clearRecipes()
     } 
     */
 }
+function parseVideoElem(videoElem) {
+    
+    var postVideoElem = videoElem.querySelector('.video iframe');
+    var videoURL = postVideoElem.src;
+
+    var video = {
+      name: videoElem.getAttribute('data-name'),
+      type: videoElem.getAttribute('data-type'),
+      url:  videoURL
+    };
+  
+    return video;
+  
+  }
+window.addEventListener('DOMContentLoaded', function(){
+
+    
+    var filterExButton = document.getElementById('filter-update-button');
+
+    filterExButton.addEventListener('click', function(videoArray) {
+        
+    var videoElems =document.getElementsByClassName('exercise-video')
+    for (var i = 0; i < videoElems.length; i++) 
+    {
+        
+       videoArray.push(parseVideoElem(videoElems[i]));
+       console.log(videoArray[i].type)
+    
+    }
+        clearExercises()
+        var Videos=document.getElementById('exercise-videos')
+        var muscleType= document.querySelectorAll("#filter-exercise input:checked")
+        var values = [];
+        for (var i=0;i<muscleType.length;i++) 
+        {
+            values.push(muscleType[i].value)
+        }
+        console.log(videoArray.length)
+        for(var i=0; i<30;i++)
+        {
+            console.log('inside for loop')
+            if(values.length!==0 && !values.includes(videoArray[i].type))
+            {
+                continue
+            }
+            
+           insertVideo(videoArray[i].name, videoArray[i].url, videoArray[i].type)
+        }
+        toggleExerciseFilter()
+    });
+});
